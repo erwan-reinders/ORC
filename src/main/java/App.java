104262df25cd7eb2869ca822bd9995da5038db42
Ange <ange.clement@etu.umontpellier.fr>
2021-10-08 16:32:39 +0200

    ouih

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,28 @@
-public class App {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
 
+public class App extends JFrame{
+
+	public App(){
+	    setTitle("Orc");
+	    setSize(400, 400);
+	    setVisible(true);
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-
+	
+	
+	@Override
+	public void paint(Graphics g) {
+	    Graphics2D g2d = (Graphics2D) g;
+	    //TODO On dessine tout
+	}
+	
+	public static void main(String[] args) {
+	
+	   new App();
+	
+	}
 }
--- src/main/java/Orc.java
+++ src/main/java/Orc.java
@@ -1,5 +1,10 @@
-public class Orc extends Agent {
+import java.awt.Graphics2D;
+
+public class Orc extends Agent implements Drawable{
     /*Classe abstraite symbolisant un Orc*/
     //Idée : un orc est un agent de notre simulation, et va donc, en plus de posséder des caractéristiques
     // d'un agent, va également posséder des statistiques propres et qui vont varier d'un agent à un autre
+	public void Draw(Graphics2D g2d) {
+		
+	}
 }
