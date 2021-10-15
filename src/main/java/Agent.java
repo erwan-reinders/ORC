d305f4088653f51d87427b39e0e0e5077ebc0d6b
Julian Lecocq-Mage <70908368+julmg@users.noreply.github.com>
2021-10-15 15:46:38 +0200

    Ajout liste d'actions

--- src/main/java/Agent.java
+++ src/main/java/Agent.java
@@ -1,5 +1,30 @@
+
+import java.util.ArrayList;
+
 public abstract class Agent {
     /*Classe abstraite symbolisant un Agent*/
-    public Agent(){}
+    private ArrayList<Action> actions;
+
+
+    public Agent(ArrayList<Action> actions){
+        this.actions = actions;
+    }
+    
+    
+    
+    public void addAction(Action action){
+        if(!actions.contains(action)){
+            actions.add(action);
+        }
+    }
+    
+    public void removeAction(Action action){
+        actions.remove(action);
+    }
+    
+    public ArrayList<Action> getActions() {
+        return actions;
+    }
+    
 
 }
